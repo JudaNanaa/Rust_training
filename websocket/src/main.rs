8abd8bf9635bfad3b4b79671a17eac63c411163a
Judanana <moussaadamou898@gmail.com>
2024-12-23 03:42:10 +0100

    some code

--- websocket/src/main.rs
+++ websocket/src/main.rs
@@ -1,17 +1,31 @@
-use tokio::{io::AsyncReadExt, net::TcpSocket};
 use std::io;
 
-const MAX_CLIENT: u32 = u32::max_value();
+use tokio::{io::AsyncReadExt, net::{TcpListener, TcpSocket}};
 
-#[tokio::main]
-async fn main() -> io::Result<()> {
-    let addr = "127.0.0.1:8080".parse().unwrap();
 
+fn set_server() -> Result<TcpListener, io::Error>
+{
+	let addr = "127.0.0.1:8080".parse().unwrap();
+	let max_client = u32::max_value();
     let socket = TcpSocket::new_v4()?;
     socket.set_reuseaddr(true)?;
     socket.bind(addr)?;
+    let listener = socket.listen(max_client)?;
 
-    let listener = socket.listen(MAX_CLIENT)?;
+	return Ok(listener);
+}
+
+
+
+#[tokio::main]
+async fn main() {
+	let listener = match set_server() {
+		Ok(value) => value,
+		Err(error) => {
+			println!("Error: {}", error);
+			return ;
+		},
+	};
 
 	let mut str = String::new();
 
@@ -20,12 +34,17 @@ async fn main() -> io::Result<()> {
 			Ok((mut client_stream, addr)) => {
 				println!("new client: {:?}", addr);
 				let mut buffer = [0; 1024];
-				client_stream.read(&mut buffer).await?;
-				str = String::from_utf8_lossy(buffer.as_slice()).to_string();
+				let nb_read = match client_stream.read(&mut buffer).await {
+					Ok(n) => n,
+					Err(error) => {
+						println!("Error: {}", error);
+						return;
+					},
+				};
+				str = String::from_utf8_lossy(&buffer[..nb_read]).to_string();
 			},
 			Err(e) => println!("couldn't get client: {:?}", e),
 		}
 		println!("Str content == \n{str}");
 	}
-	Ok(())
 }
